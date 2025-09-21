844a3ed7f2aff9f3bfe7d0f4169db6f9d37b60d6
IamSavitha <savitha.vijayarangan09@gmail.com>
2025-09-21 11:55:31 -0700

    updated views(HTML works from here) , and route it in main.js, running from app.js

--- authentication/app.js
+++ authentication/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // View engine
 app.set('view engine', 'ejs');
---
+++ authentication/routes/main.js
@@ -0,0 +1,17 @@
+// routes/main.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Home • ADS-SJSU' });
+});
+
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'About • ADS-SJSU' });
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact • ADS-SJSU' });
+});
+
+module.exports = router;
